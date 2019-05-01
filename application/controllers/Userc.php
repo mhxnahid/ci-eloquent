3ddaf6cc65bde3836e6262498aa7c1279fb623c3
mhxnahid <mhnahid@live.com>
2019-05-01 11:17:33 +0600

    Git init

---
+++ application/controllers/Userc.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class Userc extends CI_Controller {
+	function __construct()
+    {
+		parent::__construct();
+		
+		$this->load->model('user');
+    }
+    
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+	
+	public function create()
+	{
+		$data = [
+			'email' => 'kiki@fi.com'
+		];
+		User::create($data);
+	}
+	
+	public function list()
+	{
+		$users = User::first();
+		print_r($users);
+	}
+	
+	public function update()
+	{
+		$users = User::where('email', 'kiki@fi.com')->first();
+		$users->email = 'dix@er.er';
+		$users->save();
+	}
+	
+	public function build()
+	{
+		Capsule::schema()->create('users', function ($table) {
+			$table->increments('id');
+			$table->string('email')->unique();
+			$table->timestamps();
+		});
+	}
+}
---
+++ application/models/User.php
@@ -0,0 +1,10 @@
+<?php
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class User extends Eloquent{
+	protected $table = 'users';
+	
+	protected $fillable = [
+        'email'
+    ];
+}
